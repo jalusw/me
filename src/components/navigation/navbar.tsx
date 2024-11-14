5db33a26d80fdc9f874f57fa5992172c27a1226f
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-11-14 12:55:46 +0800

    feat: update navbar  content

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -3,22 +3,21 @@ import {
   LanguagesIcon,
   MenuIcon,
   MoonIcon,
-  SunIcon,
   XIcon,
 } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 py-4 backdrop-blur">
+    <nav className="sticky top-0 py-4 backdrop-blur-2xl">
       <div className="container">
         <div className="flex items-center justify-between">
           <div className="flex flex-1 items-center">
             <a className="text-2xl font-bold" href="/">
-              Jalu
+              jalusw
             </a>
-            <ul className="absolute bottom-0 left-0 right-0 top-0 flex hidden h-[100vh] flex-col items-center justify-center space-y-16 bg-white lg:static lg:flex lg:flex-row lg:items-baseline lg:space-x-8 lg:space-y-0 lg:h-auto">
+            <ul className="absolute bottom-0 left-0 right-0 top-0 flex hidden h-[100vh] flex-col items-center justify-center space-y-16 lg:bg-none lg:static lg:flex lg:flex-row lg:items-baseline lg:space-x-8 lg:space-y-0 lg:h-auto">
               <li className="absolute right-0 top-0 mr-8 mt-8 lg:hidden">
-                <Button variant="ghost">
+                <Button variant="ghost" title="close navbar">
                   <XIcon />
                 </Button>
               </li>
@@ -26,17 +25,11 @@ export default function Navbar() {
                 <a href="/">Home</a>
               </li>
               <li>
-                <a href="/services">Contact</a>
-              </li>
-              <li>
-                <a href="/services">Services</a>
-              </li>
-              <li>
                 <a href="/blogs">Blogs</a>
               </li>
             </ul>
           </div>
-          <Button variant="ghost" className="block lg:hidden">
+          <Button variant="ghost" className="block lg:hidden" title="open navbar">
             <MenuIcon />
           </Button>
           <div className="hidden lg:flex lg:space-x-2">
@@ -45,6 +38,8 @@ export default function Navbar() {
             </Button>
             <Button variant="ghost">
               <LanguagesIcon />
+            </Button>
+            <Button variant="ghost">
             </Button>
           </div>
         </div>
