553caac35ef9231ee7663cd858496e82fba6d7cc
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-11-22 15:34:11 +0800

    feat: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +12,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    mdx(),
   ],
 });
