005433e0541611a80e3d259f29368213d900f6b7
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-11-22 15:34:30 +0800

    feat: add first blog content, set layout to large screeb

--- src/components/navigation/footer.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return <footer>
-    <div className="container text-center py-2">
-      <p className="text-slate-700 text-sm">© 2024 Jalu Satriani Wijaya</p>
-    </div>
-  </footer>
-}
--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -9,13 +9,13 @@ export default function Navbar() {
   const toggleShowMenu = () => setShowMenu(!showMenu);
 
   return (
-    <nav>
-      <div className="container flex items-center justify-between py-6">
+    <nav className="border">
+      <div className="container flex max-w-screen-lg items-center justify-between py-6">
         <a className="text-2xl font-bold" href="/">
           jalusw
         </a>
         <div
-          className={`fixed bottom-0 right-0 top-0 flex min-h-screen w-full flex-col bg-white p-4 transition-all dark:bg-black lg:static lg:min-h-0 lg:flex-row lg:bg-transparent lg:p-0 ${showMenu ? "left-0" : "left-[-100%]"} lg:transition-none`}
+          className={`fixed bottom-0 right-0 top-0 flex min-h-screen w-full flex-col bg-white p-4 transition-all dark:bg-neutral-950 lg:static lg:min-h-0 lg:flex-row lg:bg-transparent lg:p-0 ${showMenu ? "left-0" : "left-[-100%]"} lg:transition-none`}
         >
           <Button
             className="self-end lg:hidden"
@@ -24,15 +24,21 @@ export default function Navbar() {
           >
             <XIcon />
           </Button>
-          <ul className="mt-8 flex w-full flex-col items-center justify-center gap-y-8 self-center lg:mt-0 lg:flex-row lg:gap-x-10 lg:gap-y-0">
+          <ul className="mt-8 flex w-full flex-col items-center justify-center gap-y-16 self-center lg:mt-0 lg:flex-row lg:gap-x-10 lg:gap-y-0">
             <li>
-              <a href="/">Home</a>
+              <a className="lg:text-md text-xl" href="/">
+                Home
+              </a>
             </li>
             <li>
-              <a href="/blog">Blog</a>
+              <a className="lg:text-md text-xl" href="/blog">
+                Blog
+              </a>
             </li>
             <li>
-              <a href="/works">Works</a>
+              <a className="lg:text-md text-xl" href="/works">
+                Works
+              </a>
             </li>
           </ul>
         </div>
